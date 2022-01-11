292bb3ec11fd096ea066d5925d978948383f66e1
Jutta Koulen <jkoulen@gmail.com>
2022-01-11 20:55:05 +0100

    Added Dictionary.js and .css
    Added search

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-        Hello
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Reni Koulen</small>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`You are searching for ${keyword}`);
+  }
+
+  function handleKeywordChange(event) {
+    event.preventDefault();
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          onChange={handleKeywordChange}
+          //placeholder="What are you looking for?"
+          //autofocus="on"
+        />
+      </form>
+    </div>
+  );
+}
