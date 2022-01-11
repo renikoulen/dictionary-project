b338af96b8e5ceb1e63a6d6a62e1ebd63027e762
Jutta Koulen <jkoulen@gmail.com>
2022-01-11 17:15:05 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.png";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+        Hello
+      </header>
+    </div>
+  );
+}
