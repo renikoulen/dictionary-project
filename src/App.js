d6db33f21ede253f4a2686b0048698c2c0c3d244
Jutta Koulen <jkoulen@gmail.com>
2022-01-24 09:36:17 +0100

    Logo changed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.png";
+import logo from "./dictionary-logo.png";
 import Dictionary from "./Dictionary";
 import "./App.css";
 
@@ -7,7 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <img src={logo} className="App-logo img-fluid " alt="logo" />
         </header>
         <main>
           <Dictionary defaultKeyword="sunset" />
